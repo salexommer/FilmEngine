eb37bddd15cee432a5f8f834b2e6da9e3b06828a
salexommer <salexommer@gmail.com>
2020-09-15 23:01:01 +0100

    Updates

--- filmengine/filmengine.py
+++ filmengine/filmengine.py
@@ -1,1 +1,16 @@
 # Initial filmengine script that will be split into separate modules later
+import requests
+import os
+import gzip
+
+# Script variables
+file_name = 'sample.xml'
+url = 'https://www.w3schools.com/xml/note.xml'
+
+# Download the required files
+r = requests.get(url, allow_redirects=True)
+open (os.path.join('./files/',file_name), 'wb').write(r.content)
+print(file_name + " has been downloaded.")
+
+# Decompress
+gzip.decompress(os.path.join('./files/',file_name))
