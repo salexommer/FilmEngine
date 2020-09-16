1ae78751db809158b8870270f65f9fd0c4196ca9
salexommer <salexommer@gmail.com>
2020-09-16 21:19:58 +0100

    Re-worked the working script to include Kaggle

--- filmengine/filmengine.py
+++ filmengine/filmengine.py
@@ -1,25 +1,12 @@
 # Initial filmengine script that will be split into separate modules later
-import requests
-import os
-import gzip
+from kaggle.api.kaggle_api_extended import KaggleApi
+api = KaggleApi()
+api.authenticate()
 
-# Script variables
-archive_name = 'enwiki-latest-abstract.xml.gz'
-url = 'https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-abstract.xml.gz'
+ Download all files of a dataset
+# Signature: dataset_download_files(dataset, path=None, force=False, quiet=True, unzip=False)
+api.dataset_download_files('avenn98/world-of-warcraft-demographics')
 
-# Download the required files
-r = requests.get(url, allow_redirects=True)
-open (os.path.join('./files/',archive_name), 'wb').write(r.content)
-
-print(archive_name + " has been downloaded.")
-
-# Decompress
-input = gzip.GzipFile("./files/" + archive_name, 'rb')
-s = input.read()
-input.close()
-
-output = open("./files/" + archive_name, 'wb')
-output.write(s)
-output.close()
-
-print("The file has been decompressed")
+# downoad single file
+#Signature: dataset_download_file(dataset, file_name, path=None, force=False, quiet=True)
+api.dataset_download_file('avenn98/world-of-warcraft-demographics','WoW Demographics.csv')
