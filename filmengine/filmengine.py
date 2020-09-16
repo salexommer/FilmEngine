47ea332fa9266bf5033483e88957a2daa272dce5
salexommer <salexommer@gmail.com>
2020-09-16 21:17:25 +0100

    Wipe the working script

--- filmengine/filmengine.py
+++ filmengine/filmengine.py
@@ -4,13 +4,22 @@ import os
 import gzip
 
 # Script variables
-file_name = 'sample.xml'
-url = 'https://www.w3schools.com/xml/note.xml'
+archive_name = 'enwiki-latest-abstract.xml.gz'
+url = 'https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-abstract.xml.gz'
 
 # Download the required files
 r = requests.get(url, allow_redirects=True)
-open (os.path.join('./files/',file_name), 'wb').write(r.content)
-print(file_name + " has been downloaded.")
+open (os.path.join('./files/',archive_name), 'wb').write(r.content)
+
+print(archive_name + " has been downloaded.")
 
 # Decompress
-gzip.decompress(os.path.join('./files/',file_name))
+input = gzip.GzipFile("./files/" + archive_name, 'rb')
+s = input.read()
+input.close()
+
+output = open("./files/" + archive_name, 'wb')
+output.write(s)
+output.close()
+
+print("The file has been decompressed")
