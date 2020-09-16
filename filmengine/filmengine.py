cda0411676c9278958a35e7c3c2455c6f405abd1
salexommer <salexommer@gmail.com>
2020-09-16 21:54:06 +0100

    Kaggle creds set up

--- filmengine/filmengine.py
+++ filmengine/filmengine.py
@@ -3,10 +3,10 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 api = KaggleApi()
 api.authenticate()
 
- Download all files of a dataset
+ #Download all files of a dataset
 # Signature: dataset_download_files(dataset, path=None, force=False, quiet=True, unzip=False)
 api.dataset_download_files('avenn98/world-of-warcraft-demographics')
 
-# downoad single file
+# Download single file
 #Signature: dataset_download_file(dataset, file_name, path=None, force=False, quiet=True)
 api.dataset_download_file('avenn98/world-of-warcraft-demographics','WoW Demographics.csv')
